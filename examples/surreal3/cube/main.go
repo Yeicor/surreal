420334bf139459d0da2e521cb4ff770acad173b6
Yeicor <yeicor1212@gmail.com>
2022-01-13 18:33:03 +0100

    More stable outputs

--- examples/surreal3/cube/main.go
+++ examples/surreal3/cube/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/deadsy/sdfx/sdf"
 	"log"
 	"math"
+	"strconv"
 	"time"
 )
 
@@ -13,10 +14,12 @@ func main() {
 	s, _ := sdf.Box3D(sdf.V3{X: 1, Y: 1, Z: 1}, 0)
 	// TODO: Apply some transforms to make the problem harder
 	//s = sdf.Transform3D(s, sdf.Translate3d(sdf.V3{X: 4, Y: 1, Z: -4}).Mul(sdf.Rotate3d(sdf.V3{X: 1, Y: 1, Z: 1}, math.Pi/8)))
-	//s = sdf.ScaleUniform3D(s, 2)
+	s = sdf.ScaleUniform3D(s, 2)
 
 	startTime := time.Now()
-	alg := surreal3.NewSimple(math.Pi/4, 1e-1, sdf.V3i{1, 1, 1})
+	//for step := 0.01; step > 1e-6; step *= 0.1 {
+	step := 0.01
+	alg := surreal3.NewSimple(math.Pi/4, step, sdf.V3i{1, 1, 1})
 	triangles := alg.Run(s)
 	log.Println("Generated", len(triangles), "output triangles in", time.Since(startTime))
 	for _, triangle := range triangles {
@@ -27,6 +30,7 @@ func main() {
 	render.ToSTL(s, -1, "render.stl", alg)
 
 	if len(triangles) != 12 {
-		panic("Cubes (low enough step and minAngle) are expected to render using only 12 triangles")
+		panic("Cubes (low enough step and minAngle) are expected to render using only 12 triangles, but got " + strconv.Itoa(len(triangles)))
 	}
+	//}
 }
